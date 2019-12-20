c43c12856b83c7084ad46d646ef5a3be0dd97119
Winifred Frierson <winifred.dykes@hotmail.com>
2019-12-20 02:35:37 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,19 +3,4 @@ require 'bigdecimal/math'
 
 
 
-def sieve(max)# Add  code here!
-  primes = (0..max).to_a
-  primes[0] = primes[1] = nil
-  
-  counter = 0
-  primes.each do |p|
-    next unless p
-    break if p*p > max
-    counter += 1
-    
-    (p*p).step(max,p) { |m| primes[m] = nil }
-  end
-  
-  puts  "Solved for #{max} in #{counter} steps."
-  primes.compact
-end
+

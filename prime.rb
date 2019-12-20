aeaed8aa90669b8bdf639edf057fd6ccf92f5f72
Winifred Frierson <winifred.dykes@hotmail.com>
2019-12-20 02:39:31 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,4 +1,14 @@
-
+def prime?(number)
+  start = 2
+  if number > 1
+    range = (start..number-1).to_a
+    range.none? do |num_to_test|
+      number % num_to_test == 0
+    end
+  else
+    false
+  end
+end
 
 
 

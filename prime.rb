9148290af57c471c25bf5b0d29a3f252c11cd04d
Winifred Frierson <winifred.dykes@hotmail.com>
2019-12-20 02:23:37 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,7 @@
 require 'benchmark'
 require 'bigdecimal/math'
 
-puts benchmark.measure { BigMath.prime(18_000) }
+puts benchmark.measure { bigMath.pi(18_000) }
 
 
 def prime?(num)# Add  code here!
